e9205dfa5fa358f54e5cb276fe885748867f81f0
Nina <lnina@00066gmail.com>
2025-04-29 16:50:24 +0200

    element 1

--- dist/js/theme.js
+++ dist/js/theme.js
@@ -1,2 +1,15 @@
 
+const swiper = new Swiper('.swiper', {
+    loop: true, // pour que ça tourne en boucle
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 4, // Combien de slides visibles
+    spaceBetween: 10, // Espace entre les slides en px
+  });
   
