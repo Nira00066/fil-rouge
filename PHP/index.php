473706354d1198f12995be84f3b5ec015b45a3d9
Nina <lnina@00066gmail.com>
2025-05-28 09:50:30 +0200

    creation carousel 2

--- PHP/index.php
+++ PHP/index.php
@@ -1,10 +1,24 @@
 <?php require_once('config/settings.php'); ?>
 <?php
 // Requête pour récupérer les events avec leur nom de catégorie
-$sql = "SELECT events.*, events_categories.name AS category
+$sql1 = "SELECT events.*, events_categories.name AS category
         FROM events
         JOIN events_categories ON events.id_category = events_categories.id
         WHERE id_category=1
+        ORDER BY date_start ASC";
+
+$stmt1 = $pdo->query($sql1);
+$eventscat1 = $stmt1->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<!-- La recupere diretement la cat 1 -->
+
+<?php
+// Requête pour récupérer les events avec leur nom de catégorie
+$sql = "SELECT events.*, events_categories.name AS category
+        FROM events
+        JOIN events_categories ON events.id_category = events_categories.id
+        WHERE id_category= 4
         ORDER BY date_start ASC";
 
 $stmt = $pdo->query($sql);
@@ -18,7 +32,7 @@ $events = $stmt->fetchAll(PDO::FETCH_ASSOC);
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Les pépites du coins</title>
   <meta name="description" content="Les pépites du coins" />
-  <link rel="stylesheet" href="<?php echo css_dir; ?>theme.css?<?=time();?>" />
+  <link rel="stylesheet" href="<?php echo css_dir; ?>theme.css?<?= time(); ?>" />
   <link rel="icon" type="image/x-icon" href="<?php echo img_dir; ?>/pepite-1.png" />
   <link
     href="https://fonts.googleapis.com/icon?family=Material+Icons"
@@ -69,38 +83,67 @@ $events = $stmt->fetchAll(PDO::FETCH_ASSOC);
       <h2 class="carosel_titre">Thème : Auto/Moto</h2>
 
       <div class="swiper">
-          <div class="swiper-button-prev"></div>
-          <div class="swiper-button-next"></div>
-          <div class="swiper-wrapper">
-            <?php foreach ($events as $event) { ?>
-              <div class="swiper-slide">
-                <article class="card-article">
-                  <div class="card-container">
-                    <div class="card-img">
-                      <img src="<?php echo img_dir; ?>events/small/<?php echo $event['image']; ?>" alt="<?php echo $event['title']; ?>">
-                    </div>
-                    <div class="card-text">
-                      <h3><?php echo $event['title']; ?></h3>
-                      <div class="card-category">
-                        <span class="category-card"><?php echo $event['category']; ?></span>
-                        <span class="like-card">❤️</span>
-                      </div>
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+        <div class="swiper-wrapper">
+          <?php foreach ($eventscat1 as $event) { ?>
+            <div class="swiper-slide">
+              <article class="card-article">
+                <div class="card-container">
+                  <div class="card-img">
+                    <img src="<?php echo img_dir; ?>events/small/<?php echo $event['image']; ?>" alt="<?php echo $event['title']; ?>">
+                  </div>
+                  <div class="card-text">
+                    <h3><?php echo $event['title']; ?></h3>
+                    <div class="card-category">
+                      <span class="category-card"><?php echo $event['category']; ?></span>
+                      <span class="like-card">❤️</span>
                     </div>
                   </div>
-                </article>
-              </div>
-            <?php } ?>
-          </div>
+                </div>
+              </article>
+            </div>
+          <?php }
+          ?>
+        </div>
       </div>
     </section>
 
-  </div>
+
+    <h2 class="carosel_titre">Thème : Fripe</h2>
+
+    <div class="swiper">
+      <div class="swiper-button-prev"></div>
+      <div class="swiper-button-next"></div>
+      <div class="swiper-wrapper">
+        <?php foreach ($events as $event) { ?>
+          <div class="swiper-slide">
+            <article class="card-article">
+              <div class="card-container">
+                <div class="card-img">
+                  <img src="<?php echo img_dir; ?>events/small/<?php echo $event['image']; ?>" alt="<?php echo $event['title']; ?>">
+                </div>
+                <div class="card-text">
+                  <h3><?php echo $event['title']; ?></h3>
+                  <div class="card-category">
+                    <span class="category-card"><?php echo $event['category']; ?></span>
+                    <span class="like-card">❤️</span>
+                  </div>
+                </div>
+              </div>
+            </article>
+          </div>
+        <?php } ?>
+      </div>
+    </div>
+    </section>
+    </div>
 
 
-</div>
+    </div>
   </main>
   <?php include('layout/footer.php') ?>
-  <script defer src="<?php echo js_dir ?>theme.js?<?=time();?>"></script>
+  <script defer src="<?php echo js_dir ?>theme.js?<?= time(); ?>"></script>
 </body>
 
 </html>
